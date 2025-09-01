54408f09f9584d2f4a8bacd05566b0ce9e7b3f6a
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-01 10:48:37 -0300

    modelo básico de article_tags

---
+++ src/models/articleTag.model.js
@@ -0,0 +1,14 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/batabase.js";
+
+const article_tag = sequelize.define(
+  "article_tag",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {}
+);
