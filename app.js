be04f7fa33f8efc5bc7fd0c411c018f48eb1e3f2
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-02 10:59:34 -0300

    importé los modelos para me cree las tablas

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { startDB } from "./src/config/batabase.js";
+// ----------------------------------------------------------------------------------------------------------
+import { User } from "./src/models/user.model.js";
+import { Tag } from "./src/models/tag.model.js";
+import { Profile } from "./src/models/profile.model.js";
+import { ArticleTag } from "./src/models/articleTag.model.js";
+import { Article } from "./src/models/article.model.js";
+
+// ----------------------------------------------------------------------------------------------------------
 dotenv.config();
 
 const PORT = process.env.PORT;
