05dd05d831cdc2f6eae565daaea1b2be48809473
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-02 16:11:11 -0300

    rutas

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { startDB } from "./src/config/batabase.js";
+dotenv.config();
 import { userRoute } from "./src/routes/user.route.js";
+import { profileRoute } from "./src/routes/profile.route.js";
+import { tagRoute } from "./src/routes/tag.route.js";
+import { articleRoute } from "./src/routes/article.route.js";
 // ----------------------------------------------------------------------------------------------------------
 import { User } from "./src/models/user.model.js";
 import { Tag } from "./src/models/tag.model.js";
@@ -9,13 +13,14 @@ import { Profile } from "./src/models/profile.model.js";
 import { ArticleTag } from "./src/models/articleTag.model.js";
 import { Article } from "./src/models/article.model.js";
 // ----------------------------------------------------------------------------------------------------------
-dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use("/api", userRoute);
-
+app.use("/api", profileRoute);
+app.use("/api", tagRoute);
+app.use("/api", articleRoute);
 app.listen(PORT, async () => {
   await startDB();
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
