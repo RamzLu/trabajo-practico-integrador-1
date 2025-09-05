709e97ca107a0d57516052ebf8e871f12a7d08e4
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-05 15:36:31 -0300

    rutas tag

--- src/routes/tag.route.js
+++ src/routes/tag.route.js
@@ -6,11 +6,13 @@ import {
   updateTag,
   deleteTag,
 } from "../controllers/tag.controller.js";
+import { authAdminMiddleware } from "../middleware/authAdmin.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const tagRoute = Router();
 
-tagRoute.get("/tags", getAllTag);
-tagRoute.get("/tags/:id", getTagById);
-tagRoute.post("/tags", createTag);
-tagRoute.put("/tags/:id", updateTag);
-tagRoute.delete("/tags/:id", deleteTag);
+tagRoute.get("/tags", authMiddleware, getAllTag);
+tagRoute.get("/tags/:id", authMiddleware, authAdminMiddleware, getTagById);
+tagRoute.post("/tags", authMiddleware, authAdminMiddleware, createTag);
+tagRoute.put("/tags/:id", authMiddleware, authAdminMiddleware, updateTag);
+tagRoute.delete("/tags/:id", authMiddleware, authAdminMiddleware, deleteTag);
