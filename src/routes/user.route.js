65a9adf78e882033e8e9194a170951051fe7dd61
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-05 16:01:33 -0300

    validators y middlewares user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,13 +4,43 @@ import {
   deleteUser,
   getAllUsers,
   getUserById,
-  updateUser,
+  // updateUser,
 } from "../controllers/user.controller.js";
-
+import { authAdminMiddleware } from "../middleware/authAdmin.js";
+import { authMiddleware } from "../middleware/auth.js";
+import validator from "../middleware/validator.js";
+import {
+  createUserValidation,
+  deleteUserValidation,
+  getUserByIdValidation,
+} from "../middleware/validations/user.validations.js";
 export const userRoute = Router();
 
-userRoute.get("/users", getAllUsers);
-userRoute.get("/users/:id", getUserById);
-userRoute.post("/users", createUser);
-userRoute.put("/users/:id", updateUser);
-userRoute.delete("/users/:id", deleteUser);
+userRoute.get("/users", authMiddleware, authAdminMiddleware, getAllUsers);
+
+userRoute.get(
+  "/users/:id",
+  authMiddleware,
+  authAdminMiddleware,
+  getUserByIdValidation,
+  validator,
+  getUserById
+);
+
+userRoute.post(
+  "/users",
+  authAdminMiddleware,
+  createUserValidation,
+  validator,
+  createUser
+);
+
+// userRoute.put("/users/:id", authMiddleware, authAdminMiddleware, updateUser);
+
+userRoute.delete(
+  "/users/:id",
+  authAdminMiddleware,
+  deleteUserValidation,
+  validator,
+  deleteUser
+);
