d5b22cba775dd8812d96a422b8dd04ef3e4701ca
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-05 15:59:13 -0300

    validators

--- src/routes/tag.route.js
+++ src/routes/tag.route.js
@@ -8,11 +8,45 @@ import {
 } from "../controllers/tag.controller.js";
 import { authAdminMiddleware } from "../middleware/authAdmin.js";
 import { authMiddleware } from "../middleware/auth.js";
-
+import validator from "../middleware/validator.js";
+import {
+  createTagValidations,
+  deleteTagValidations,
+  getTagByIDValidation,
+  updateTagValidations,
+} from "../middleware/validations/tag.validations.js";
 export const tagRoute = Router();
 
 tagRoute.get("/tags", authMiddleware, getAllTag);
-tagRoute.get("/tags/:id", authMiddleware, authAdminMiddleware, getTagById);
-tagRoute.post("/tags", authMiddleware, authAdminMiddleware, createTag);
-tagRoute.put("/tags/:id", authMiddleware, authAdminMiddleware, updateTag);
-tagRoute.delete("/tags/:id", authMiddleware, authAdminMiddleware, deleteTag);
+tagRoute.get(
+  "/tags/:id",
+  authMiddleware,
+  authAdminMiddleware,
+  getTagByIDValidation,
+  validator,
+  getTagById
+);
+tagRoute.post(
+  "/tags",
+  authMiddleware,
+  authAdminMiddleware,
+  createTagValidations,
+  validator,
+  createTag
+);
+tagRoute.put(
+  "/tags/:id",
+  authMiddleware,
+  authAdminMiddleware,
+  updateTagValidations,
+  validator,
+  updateTag
+);
+tagRoute.delete(
+  "/tags/:id",
+  authMiddleware,
+  authAdminMiddleware,
+  deleteTagValidations,
+  validator,
+  deleteTag
+);
