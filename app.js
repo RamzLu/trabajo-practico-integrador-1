32e935224eb60bac2a63cdd3b77e4a6a056119ff
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-08 11:35:46 -0300

    se cambio el "api" por "auth"  y el cookie-parse

--- app.js
+++ app.js
@@ -13,17 +13,19 @@ import { Tag } from "./src/models/tag.model.js";
 import { Profile } from "./src/models/profile.model.js";
 import { ArticleTag } from "./src/models/articleTag.model.js";
 import { Article } from "./src/models/article.model.js";
+import cookieParser from "cookie-parser";
 // ----------------------------------------------------------------------------------------------------------
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api", userRoute);
 app.use("/api", profileRoute);
 app.use("/api", tagRoute);
 app.use("/api", articleRoute);
 app.use("/api", articleTagRoute);
-app.use("/api", authRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, async () => {
   await startDB();
