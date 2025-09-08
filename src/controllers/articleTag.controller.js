3c7831eb6aba42a2ff9837af3d0b4203752086fa
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-08 11:25:14 -0300

    articleTag controller

--- src/controllers/articleTag.controller.js
+++ src/controllers/articleTag.controller.js
@@ -68,28 +68,20 @@ export const getArticleTagById = async (req, res) => {
 
 export const createArticleTag = async (req, res) => {
   try {
-    const newRelation = await ArticleTag.create(req.body);
-    const article_tag = await ArticleTag.findByPk(newRelation.id, {
-      attributes: {
-        exclude: ["article_id", "tag_id"],
-      },
-      include: [
-        {
-          model: Article,
-          as: "article",
-          attributes: { exclude: ["article_tag"] },
-          include: [{ model: User, as: "author" }],
-        },
-        {
-          model: Tag,
-          as: "tag",
-        },
-      ],
-    });
-    return res.status(201).json({
-      msg: "The new relationship has been successfully created.",
-      article_tag: article_tag,
-    });
+    const { article_id, tag_id } = req.body;
+    const article = await Article.findByPk(article_id);
+
+    if (!article) {
+      return res.status(404).json({ msg: "Artículo no encontrado." });
+    }
+    if (article.user_id !== req.user.id) {
+      return res
+        .status(403)
+        .json({ msg: "No tienes permiso para modificar este artículo." });
+    }
+
+    const newRelation = await ArticleTag.create({ article_id, tag_id });
+    res.status(201).json(newRelation);
   } catch (error) {
     res.status(500).json({
       mesagge: "Entered the try catch.",
@@ -137,9 +129,6 @@ export const updateArticleTag = async (req, res) => {
 export const deleteArticleTag = async (req, res) => {
   try {
     const article_tag = await ArticleTag.findByPk(req.params.id, {
-      attributes: {
-        exclude: ["article_id", "tag_id"],
-      },
       include: [
         {
           model: Article,
@@ -158,10 +147,15 @@ export const deleteArticleTag = async (req, res) => {
         mesagge: "Relationship not found",
       });
     }
-    await ArticleTag.destroy({ where: { id: req.params.id } });
+    if (ArticleTag.article.user_id !== req.user.id) {
+      return res
+        .status(403)
+        .json({ msg: "No tienes permiso para modificar este artículo." });
+    }
+    await ArticleTag.destroy();
     return res.status(200).json({
-      mesagge: "Relationship deleted successfully.",
-      article_tag: article_tag,
+      message: "Relación eliminada exitosamente.",
+      deletedRelationship: ArticleTag,
     });
   } catch (error) {
     res.status(500).json({
